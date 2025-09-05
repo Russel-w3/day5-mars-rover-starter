6476c91a62322bde807547615797360fc9f474fc
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-05 21:34:03 +0800

    refactor: rename MOVE to MOVE_FORWARD

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -5,7 +5,7 @@ public class MarsRover {
     public static final String WEST = "W";
     public static final String EAST = "E";
     public static final String SOUTH = "S";
-    public static final String MOVE = "M";
+    public static final String MOVE_FORWARD = "M";
     public static final String TURN_LEFT = "L";
     public static final String TURN_RIGHT = "R";
     private int x;
@@ -34,28 +34,28 @@ public class MarsRover {
         switch (direction) {
             case NORTH -> {
                 switch (command) {
-                    case MOVE -> y++;
+                    case MOVE_FORWARD -> y++;
                     case TURN_LEFT -> direction = WEST;
                     case TURN_RIGHT -> direction = EAST;
                 }
             }
             case EAST -> {
                 switch (command) {
-                    case MOVE -> x++;
+                    case MOVE_FORWARD -> x++;
                     case TURN_LEFT -> direction = NORTH;
                     case TURN_RIGHT -> direction = SOUTH;
                 }
             }
             case SOUTH -> {
                 switch (command) {
-                    case MOVE -> y--;
+                    case MOVE_FORWARD -> y--;
                     case TURN_LEFT -> direction = EAST;
                     case TURN_RIGHT -> direction = WEST;
                 }
             }
             case WEST -> {
                 switch (command) {
-                    case MOVE -> x--;
+                    case MOVE_FORWARD -> x--;
                     case TURN_LEFT -> direction = SOUTH;
                     case TURN_RIGHT -> direction = NORTH;
                 }
