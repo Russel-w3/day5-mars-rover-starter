2b1b5d987df35cf0fba006a40b0086fa0183f2fc
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-05 15:52:08 +0800

    feat: should report 01N when input 00N with MOVE FORWARD

---
+++ src/main/java/com/afs/tdd/MarRover.java
@@ -0,0 +1,33 @@
+package com.afs.tdd;
+
+public class MarRover {
+    private int x;
+    private int y;
+    private String direction;
+
+    public MarRover(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void report(String command) {
+        if(direction.equals("N")){
+            if(command.equals("M")){
+                y++;
+            }
+        }
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,21 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    public void should_report_01N_when_input_00N_with_M(){
+        //Given
+        int x = 0,y = 0;
+        String direction = "N",command = "M";
+        MarRover marRover = new MarRover(x, y, direction);
+        //When
+        marRover.report(command);
+        //Then
+        assertEquals(0,marRover.getX());
+        assertEquals(1,marRover.getY());
+        assertEquals(direction,marRover.getDirection());
     }
+    
 }
