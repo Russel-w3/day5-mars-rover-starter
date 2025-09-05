8e027b950f852b7d0946309ad6399f50373c5530
WANGRU3 <RUSSEL.WANG@OOCL.COM>
2025-09-05 15:54:42 +0800

    refactor: reformat code

--- src/test/java/com/afs/tdd/MarRoverTest.java
+++ src/test/java/com/afs/tdd/MarRoverTest.java
@@ -6,30 +6,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarRoverTest {
     @Test
-    public void should_report_01N_when_input_00N_with_M(){
+    public void should_report_01N_when_input_00N_with_M() {
         //Given
-        int x = 0,y = 0;
-        String direction = "N",command = "M";
+        int x = 0, y = 0;
+        String direction = "N", command = "M";
         MarRover marRover = new MarRover(x, y, direction);
         //When
         marRover.report(command);
         //Then
-        assertEquals(0,marRover.getX());
-        assertEquals(1,marRover.getY());
-        assertEquals(direction,marRover.getDirection());
+        assertEquals(0, marRover.getX());
+        assertEquals(1, marRover.getY());
+        assertEquals(direction, marRover.getDirection());
     }
 
     @Test
-    public void should_report_00W_when_input_00N_with_L(){
+    public void should_report_00W_when_input_00N_with_L() {
         //Given
-        int x = 0,y = 0;
-        String direction = "N",command = "L";
+        int x = 0, y = 0;
+        String direction = "N", command = "L";
         MarRover marRover = new MarRover(x, y, direction);
         //When
         marRover.report(command);
         //Then
-        assertEquals(0,marRover.getX());
-        assertEquals(0,marRover.getY());
-        assertEquals("W",marRover.getDirection());
+        assertEquals(0, marRover.getX());
+        assertEquals(0, marRover.getY());
+        assertEquals("W", marRover.getDirection());
     }
 }
